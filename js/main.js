16d50f5f1311fb9462e4f392e071802cc4c513b6
Marcelo <bustosmg@gmail.com>
2024-09-23 14:22:12 -0300

    base

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Este es un comentario en JavaScript. Sirve para explicar el código.
+
+// Al cargar la página, se ejecuta esta función
+window.onload = function() {
+    // Muestra una alerta con un mensaje
+    alert("¡Hola desde JavaScript!");
+  };
